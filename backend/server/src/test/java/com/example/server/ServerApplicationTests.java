20754b03624548d724b48acbd3c46a12943e032c
[Github <[Github>
2022-04-13 17:05:59 +0900

    Add : create server project

--- backend/server/src/test/java/com/example/server/ServerApplicationTests.java
+++ backend/server/src/test/java/com/example/server/ServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.test;
+package com.example.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TestApplicationTests {
+class ServerApplicationTests {
 
 	@Test
 	void contextLoads() {
